b709b198ac4c44499d354b9bcab4fd62e178b9f0
0xflim <90166461+0xflim@users.noreply.github.com>
2021-09-13 11:55:07 -0600

    Update bot.py
    added response code 429  ValueError exception handling
    
    added 24hr price change into "watching" activity
    
    added some more printing to console for each request

--- bot.py
+++ bot.py
@@ -13,17 +13,17 @@ from datetime import datetime as dt
 # Market ID goes here. This can be found by going to the token's page on
 # CoinGecko ex. --> https://www.coingecko.com/en/coins/<id>.
 ################################################################################
-MARKET_ID = 'ADD YOUR ID HERE'
+MARKET_ID = 'dopex'
 ################################################################################
 
 ################################################################################
 # Your bot's token goes here. This can be found on the Discord developers
 # portal.
 ################################################################################
-BOT_TOKEN = 'ADD YOUR TOKEN HERE'
+BOT_TOKEN = ''
 ################################################################################
 
-print('\n---------- V² DISCORD x COINGECKO BOT ----------\n')
+print('\n---------- V3 DISCORD x COINGECKO BOT ----------\n')
 
 ################################################################################
 # Sanity check for market ID.
@@ -52,22 +52,31 @@ client = Client()
 @client.event
 async def on_ready():
     errored_guilds = []
-    await client.change_presence(
-        activity=Activity(
-            name=f'{token_name}/USD on CoinGecko',
-            type=ActivityType.watching,
-        ),
-    )
+    # await client.change_presence(
+    #     activity=Activity(
+    #         name=f'CoinGecko',
+    #         type=ActivityType.watching,
+    #     ),
+    # )
     print(f'{dt.utcnow()} | Discord client is running.\n')
     while True:
         try:
-            price = requests.get(
-                f'https://api.coingecko.com/api/v3/coins/{MARKET_ID}'
-            ).json()['market_data']['current_price']['usd']
+            response = requests.get(f'https://api.coingecko.com/api/v3/coins/{MARKET_ID}')
+            print(f'{dt.utcnow()} | response status code: {response.status_code}.')
+            price = response.json()['market_data']['current_price']['usd']
+            print(f'{dt.utcnow()} | {token_name} price: {price}.')
+            pctchng = response.json()['market_data']['price_change_percentage_24h_in_currency']['usd']
+            print(f'{dt.utcnow()} | {token_name} 24hr % change: {round(pctchng,2)}%.')
             for guild in client.guilds:
                 try:
                     await guild.me.edit(
-                        nick=f'{token_name} ${round(float(price), 2)}'
+                        nick=f'{token_name} ${round(price,2):,}'
+                    )
+                    await client.change_presence(
+                        activity=Activity(
+                            name=f'24h: {round(pctchng,2)}%',
+                            type=ActivityType.watching,
+                        ),
                     )
                 except errors.Forbidden:
                     if guild not in errored_guilds:
@@ -78,6 +87,9 @@ async def on_ready():
                     print(f'{dt.utcnow()} | Unknown error: {e}.')
         except requests.exceptions.HTTPError as e:
             print(f'{dt.utcnow()} | HTTP error: {e}.')
+        except ValueError:
+            print(f'{dt.utcnow()} | ValueError due to {response.status_code}. Waiting: {response.headers["Retry-After"]}.')
+            await asyncio.sleep(response.headers["Retry-After"])
         finally:
             await asyncio.sleep(30)
 ################################################################################
