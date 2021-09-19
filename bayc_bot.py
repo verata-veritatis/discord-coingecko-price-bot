32642ace8931bc867ed5a5dd3271f495d46a8d06
0xflim <90166461+0xflim@users.noreply.github.com>
2021-09-19 17:49:17 -0600

    Add files via upload
    added simple json decode error exception handling

--- bayc_bot.py
+++ bayc_bot.py
@@ -1,28 +1,23 @@
 import asyncio
 import requests
 
-from discord import (
-    Activity,
-    ActivityType,
-    Client,
-    errors,
-)
+from discord import Activity, ActivityType, Client, errors
 from datetime import datetime as dt
 
 ################################################################################
 # Your bot's token goes here. This can be found on the Discord developers
 # portal.
 ################################################################################
-BOT_TOKEN = ''
+BOT_TOKEN = 'ODg2NDM4NzY2MzEzNjA3MTcw.YT1mbw.4Kc2KB1yVAF77s_OberrtX2Z5gE'
 ################################################################################
 
 print('\n---------- Flim\'s BAYC Discord Bot ----------\n')
 
 url = "https://api.opensea.io/api/v1/collections"
 querystring = {
-                "offset":"0"
-               ,"asset_owner":"0x5b21175a82F112C88e306A546166037d238DE684"
-               }
+    "offset": "0",
+    "asset_owner": "0x5b21175a82F112C88e306A546166037d238DE684",
+}
 
 ################################################################################
 # Start client.
@@ -43,7 +38,10 @@ async def on_ready():
         try:
             response = requests.request("GET", url, params=querystring)
             for index, primary_asset_contract in enumerate(response):
-                if response.json()[index]['primary_asset_contracts'][0]['address'] == "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d":
+                if (
+                    response.json()[index]['primary_asset_contracts'][0]['address']
+                    == "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
+                ):
                     break
             floor_price = response.json()[index]['stats']['floor_price']
             address = response.json()[index]['primary_asset_contracts'][0]['address']
@@ -53,33 +51,50 @@ async def on_ready():
 
             # print(f'{dt.utcnow()} | index: {index}.')
             print(f'{dt.utcnow()} | response status code: {response.status_code}.')
-            print(f'{dt.utcnow()} | {token_name} floor price: {floor_price}.')
-            print(f'{dt.utcnow()} | {token_name} 7d avg. price: {round(pctchng,2)}.')
+            print(f'{dt.utcnow()} | {token_name} floor price: Ξ{floor_price}.')
+            print(f'{dt.utcnow()} | {token_name} 7d avg. price: Ξ{round(pctchng,2)}.')
             for guild in client.guilds:
                 try:
-                    await guild.me.edit(
-                        nick=f'{token_name} Ξ{round(floor_price,2):,}'
-                    )
+                    await guild.me.edit(nick=f'{token_name} Ξ{round(floor_price,2):,}')
                     await client.change_presence(
                         activity=Activity(
                             name=f'7d avg.: Ξ{round(pctchng,2)}',
                             type=ActivityType.watching,
-                        ),
+                        )
                     )
                 except errors.Forbidden:
                     if guild not in errored_guilds:
-                        print(f'{dt.utcnow()} | {guild}:{guild.id} hasn\'t set '
-                              f'nickname permissions for the bot!')
+                        print(
+                            f'{dt.utcnow()} | {guild}:{guild.id} hasn\'t set '
+                            'nickname permissions for the bot!'
+                        )
                     errored_guilds.append(guild)
                 except Exception as e:
                     print(f'{dt.utcnow()} | Unknown error: {e}.')
+        except simplejson.errors.JSONDecodeError as j:
+            print(f'{dt.utcnow()} | JSONDecodeError: {j}.')
         except requests.exceptions.HTTPError as e:
             print(f'{dt.utcnow()} | HTTP error: {e}.')
         except ValueError:
-            print(f'{dt.utcnow()} | ValueError due to {response.status_code}. Waiting: {response.headers["Retry-After"]}.')
-            await asyncio.sleep(response.headers["Retry-After"])
+            print(
+                f'{dt.utcnow()} | ValueError due to '
+                f'{response.status_code}. Waiting:'
+                f' {response.headers["Retry-After"]}.'
+            )
+        except KeyError:
+            print(
+                f'{dt.utcnow()} | KeyError due to '
+                f'{response.status_code}. Waiting:'
+                f' {response.headers["Retry-After"]}.'
+            )
+            await asyncio.sleep(int(response.headers["Retry-After"]))
+        except:
+            print(f'{dt.utcnow()} | Something else happened. Pass.')
+            pass
         finally:
             await asyncio.sleep(30)
+
+
 ################################################################################
 
 ################################################################################
