25cd00260dcbb88c0ff9614031e8ed4fbebdc23b
0xflim <90166461+0xflim@users.noreply.github.com>
2021-10-12 15:13:40 -0600

    improved error handling for generic bot script

--- bot.py
+++ bot.py
@@ -1,19 +1,14 @@
 import asyncio
 import requests
 
-from discord import (
-    Activity,
-    ActivityType,
-    Client,
-    errors,
-)
+from discord import Activity, ActivityType, Client, errors
 from datetime import datetime as dt
 
 ################################################################################
 # Market ID goes here. This can be found by going to the token's page on
 # CoinGecko ex. --> https://www.coingecko.com/en/coins/<id>.
 ################################################################################
-MARKET_ID = 'dopex'
+MARKET_ID = ''
 ################################################################################
 
 ################################################################################
@@ -52,46 +47,46 @@ client = Client()
 @client.event
 async def on_ready():
     errored_guilds = []
-    # await client.change_presence(
-    #     activity=Activity(
-    #         name=f'CoinGecko',
-    #         type=ActivityType.watching,
-    #     ),
-    # )
     print(f'{dt.utcnow()} | Discord client is running.\n')
     while True:
         try:
-            response = requests.get(f'https://api.coingecko.com/api/v3/coins/{MARKET_ID}')
+            response = requests.get(
+                f'https://api.coingecko.com/api/v3/coins/{MARKET_ID}'
+            )
             print(f'{dt.utcnow()} | response status code: {response.status_code}.')
             price = response.json()['market_data']['current_price']['usd']
             print(f'{dt.utcnow()} | {token_name} price: {price}.')
-            pctchng = response.json()['market_data']['price_change_percentage_24h_in_currency']['usd']
+            pctchng = response.json()['market_data'][
+                'price_change_percentage_24h_in_currency'
+            ]['usd']
             print(f'{dt.utcnow()} | {token_name} 24hr % change: {round(pctchng,2)}%.')
             for guild in client.guilds:
                 try:
-                    await guild.me.edit(
-                        nick=f'{token_name} ${round(price,2):,}'
-                    )
+                    await guild.me.edit(nick=f'{token_name} ${round(price,5):,}')
                     await client.change_presence(
                         activity=Activity(
-                            name=f'24h: {round(pctchng,2)}%',
-                            type=ActivityType.watching,
-                        ),
+                            name=f'24h: {round(pctchng,2)}%', type=ActivityType.watching
+                        )
                     )
                 except errors.Forbidden:
                     if guild not in errored_guilds:
-                        print(f'{dt.utcnow()} | {guild}:{guild.id} hasn\'t set '
-                              f'nickname permissions for the bot!')
+                        print(
+                            f'{dt.utcnow()} | {guild}:{guild.id} hasn\'t set '
+                            'nickname permissions for the bot!'
+                        )
                     errored_guilds.append(guild)
                 except Exception as e:
                     print(f'{dt.utcnow()} | Unknown error: {e}.')
         except requests.exceptions.HTTPError as e:
             print(f'{dt.utcnow()} | HTTP error: {e}.')
-        except ValueError:
-            print(f'{dt.utcnow()} | ValueError due to {response.status_code}. Waiting: {response.headers["Retry-After"]}.')
-            await asyncio.sleep(response.headers["Retry-After"])
+        except ValueError as e:
+            print(f'{dt.utcnow()} | ValueError: {e}.')
+        except TypeError as e:
+            print(f'{dt.utcnow()} | TypeError: {e}.')
         finally:
             await asyncio.sleep(30)
+
+
 ################################################################################
 
 ################################################################################
