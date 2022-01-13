71a665b1ca8bc0ec0603183ad23d8f332d889608
0xflim <90166461+0xflim@users.noreply.github.com>
2022-01-13 12:19:22 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,23 +1,46 @@
 import asyncio
-import requests
-from bs4 import BeautifulSoup
+import requests  # pip install requests
+
+# import cfscrape
+from bs4 import BeautifulSoup  # pip install beatifulsoup4
+import pandas as pd  # pip install pandas
 
 from discord import Activity, ActivityType, Client, errors
 from datetime import datetime as dt
-
 
 print("\n---------- Flim's DPX Bridgoor Discord Bot ----------\n")
 
 token_name = "bridgoor"
 # token_name = "halloween"
 
-response = requests.get(f"https://tofunft.com/collection/dopex-{token_name}/items")
-print(f"{dt.utcnow()} | response status code: {response.status_code}.")
-soup = BeautifulSoup(
-    response.content, "html5lib"
-)  # If this line causes an error, run 'pip install html5lib' or install html5lib
+url = f"https://tofunft.com/collection/dopex-{token_name}/items"
+
+tables = pd.read_html(url)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.contetn, "html.parser")
 
 print(soup)
+
+# scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
+# # Or: scraper = cfscrape.CloudflareScraper()  # CloudflareScraper inherits from requests.Session
+# soup = scraper.get(
+#     f"https://tofunft.com/collection/dopex-{token_name}/items"
+# ).content  # => "<!DOCTYPE html><html><head>..."
+
+# print(soup)
+
+# response = requests.get(f"https://tofunft.com/collection/dopex-{token_name}/items")
+# print(f"{dt.utcnow()} | response status code: {response.status_code}.")
+# soup = BeautifulSoup(
+#     response.content, "html5lib"
+# )  # If this line causes an error, run 'pip install html5lib' or install html5lib
+
+# print(soup)
 
 # blocks = soup.find("span", attrs={"id": "blockCount"})
 # block_stats = blocks.attrs["title"].split("Blocks (")[1]
