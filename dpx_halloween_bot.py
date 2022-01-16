0d252de588a6b895747aee6e2d107581c216e39e
0xflim <90166461+0xflim@users.noreply.github.com>
2022-01-16 10:36:28 -0700

    removed discord token

--- dpx_halloween_bot.py
+++ dpx_halloween_bot.py
@@ -10,7 +10,7 @@ from datetime import datetime as dt
 # Your bot's token goes here. This can be found on the Discord developers
 # portal.
 ################################################################################
-BOT_TOKEN = "OTMyMzI0OTcxODY1MzA5MTk0.YeRVSw.mKKh3zc1XdjlfEPgEvzr42TPWZg"
+BOT_TOKEN = ""
 ################################################################################
 # token_name = "bridgoor"
 token_name = "halloween"
