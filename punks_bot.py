a6ad2bd635c507c69a64398f4aa0501e35d275d7
0xflim <90166461+0xflim@users.noreply.github.com>
2022-01-17 16:57:20 -0700

    Add files via upload

--- punks_bot.py
+++ punks_bot.py
@@ -2,29 +2,24 @@ import asyncio
 import requests
 from bs4 import BeautifulSoup
 
-from discord import (
-    Activity,
-    ActivityType,
-    Client,
-    errors,
-)
+from discord import Activity, ActivityType, Client, errors
 from datetime import datetime as dt
 
 ################################################################################
 # Your bot's token goes here. This can be found on the Discord developers
 # portal.
 ################################################################################
-BOT_TOKEN = ''
+BOT_TOKEN = ""
 ################################################################################
 
-print('\n---------- Flim\'s CryptoPunks Discord Bot ----------\n')
+print("\n---------- Flim's CryptoPunks Discord Bot ----------\n")
 
-token_name = 'PUNK'
+token_name = "PUNK"
 
 ################################################################################
 # Start client.
 ################################################################################
-print(f'{dt.utcnow()} | Starting Discord client.')
+print(f"{dt.utcnow()} | Starting Discord client.")
 client = Client()
 ################################################################################
 
@@ -35,49 +30,55 @@ client = Client()
 @client.event
 async def on_ready():
     errored_guilds = []
-    print(f'{dt.utcnow()} | Discord client is running.\n')
+    print(f"{dt.utcnow()} | Discord client is running.\n")
     while True:
         try:
-            response = requests.get(f'https://www.larvalabs.com/cryptopunks')
-            print(f'{dt.utcnow()} | response status code: {response.status_code}.')
-            soup = BeautifulSoup(response.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-            punk_stats = soup.findAll('div', attrs = {'class':'col-md-4 punk-stat'})
-            
+            response = requests.get(f"https://www.larvalabs.com/cryptopunks")
+            print(f"{dt.utcnow()} | response status code: {response.status_code}.")
+            soup = BeautifulSoup(
+                response.content, "html5lib"
+            )  # If this line causes an error, run 'pip install html5lib' or install html5lib
+            punk_stats = soup.findAll("div", attrs={"class": "col-md-4 punk-stat"})
+
             # in case we want to get other stats, we iterate through all punk stats here
             # for index, stat in enumerate(punk_stats):
             #   print(punk_stats[index].text)
-            
+
             floor = punk_stats[0].b
-            split = floor.string.split(' ETH ')
-            eth_floor = 'Ξ'+split[0]
-            usd_floor = split[1].lstrip('(').rstrip(' USD)')
-            print(f'{dt.utcnow()} | {token_name} floor: {eth_floor}.')
-            print(f'{dt.utcnow()} | {token_name} floor: {usd_floor}.')
+            split = floor.string.split(" ETH ")
+            eth_floor = "Ξ" + split[0]
+            usd_floor = split[1].lstrip("(").rstrip(" USD)")
+            print(f"{dt.utcnow()} | {token_name} floor: {eth_floor}.")
+            print(f"{dt.utcnow()} | {token_name} floor: {usd_floor}.")
             for guild in client.guilds:
                 try:
-                    await guild.me.edit(
-                        nick=f'{token_name} {eth_floor}'
-                    )
+                    await guild.me.edit(nick=f"{token_name} {eth_floor}")
                     await client.change_presence(
                         activity=Activity(
-                            name=f'in USD: {usd_floor}',
-                            type=ActivityType.watching,
-                        ),
+                            name=f"in USD: {usd_floor}", type=ActivityType.watching
+                        )
                     )
                 except errors.Forbidden:
                     if guild not in errored_guilds:
-                        print(f'{dt.utcnow()} | {guild}:{guild.id} hasn\'t set '
-                              f'nickname permissions for the bot!')
+                        print(
+                            f"{dt.utcnow()} | {guild}:{guild.id} hasn't set "
+                            "nickname permissions for the bot!"
+                        )
                     errored_guilds.append(guild)
                 except Exception as e:
-                    print(f'{dt.utcnow()} | Unknown error: {e}.')
+                    print(f"{dt.utcnow()} | Unknown error: {e}.")
         except requests.exceptions.HTTPError as e:
-            print(f'{dt.utcnow()} | HTTP error: {e}.')
-        except ValueError:
-            print(f'{dt.utcnow()} | ValueError due to {response.status_code}. Waiting: {response.headers["Retry-After"]}.')
-            await asyncio.sleep(response.headers["Retry-After"])
+            print(f"{dt.utcnow()} | HTTP error: {e}.")
+        except ValueError as e:
+            print(f"{dt.utcnow()} | ValueError: {e}.")
+        except TypeError as e:
+            print(f"{dt.utcnow()} | TypeError: {e}.")
+        except Exception as e:
+            print(f"{dt.utcnow()} | Unknown error: {e}.")
         finally:
             await asyncio.sleep(30)
+
+
 ################################################################################
 
 ################################################################################
