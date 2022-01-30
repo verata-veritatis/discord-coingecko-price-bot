b927fce50646e43e93f828e69dd3f12872542494
0xflim <0xflim@pm.me>
2022-01-29 17:58:24 -0700

    fixed epoch for dpx ssov

--- dpx_ssov_tvl_bot.py
+++ dpx_ssov_tvl_bot.py
@@ -5,10 +5,10 @@ from discord import Activity, ActivityType, Client, errors
 from datetime import datetime as dt
 
 ################################################################################
-token_name = 'dpx'
-contract = token_name + '-ssov'
-epoch = 5
-epoch_end = '28-Jan'
+token_name = "dpx"
+contract = token_name + "-ssov"
+epoch = 4
+epoch_end = "28-Jan"
 ################################################################################
 
 ################################################################################
@@ -38,9 +38,9 @@ async def on_ready():
     while True:
         try:
             response = requests.get(
-                            f"https://api.dopex.io/api/v1/tvl?include={contract}"
-                        )
-            tvl = round(float(response.json()["tvl"])/1000000,2)
+                f"https://api.dopex.io/api/v1/tvl?include={contract}"
+            )
+            tvl = round(float(response.json()["tvl"]) / 1000000, 2)
             print(f"{dt.utcnow()} | response status code: {response.status_code}.")
             print(f"{dt.utcnow()} | {contract} tvl: ${tvl:,}M.")
             for guild in client.guilds:
@@ -48,7 +48,8 @@ async def on_ready():
                     await guild.me.edit(nick=f"{token_name.upper()} ${tvl:,}M")
                     await client.change_presence(
                         activity=Activity(
-                            name=f"Epoch: {epoch} | End: {epoch_end}", type=ActivityType.watching
+                            name=f"Epoch: {epoch} | End: {epoch_end}",
+                            type=ActivityType.watching,
                         )
                     )
                 except errors.Forbidden:
